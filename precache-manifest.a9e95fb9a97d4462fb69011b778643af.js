cb595f889a3dd7e5cf22d350fabce4c25014f9c4
art1shok <kvitkaart554@gmail.com>
2019-12-06 09:28:20 +0200

    Updates

---
+++ precache-manifest.a9e95fb9a97d4462fb69011b778643af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "915c2bcf9924b16449ec4d01c1a9d22c",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "581a360130659d0a7a38",
+    "url": "/react_static-list-of-posts/static/css/main.8a3c786c.chunk.css"
+  },
+  {
+    "revision": "3485fa9145fb43f72c73",
+    "url": "/react_static-list-of-posts/static/js/2.506750e1.chunk.js"
+  },
+  {
+    "revision": "581a360130659d0a7a38",
+    "url": "/react_static-list-of-posts/static/js/main.4fbe6b44.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
